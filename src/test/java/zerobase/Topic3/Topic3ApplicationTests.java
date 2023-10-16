6829597b19b8fb5a3375f4b93acfc1dd1d4f58fc
YoonHee <135304661+namyoonhee@users.noreply.github.com>
2023-10-16 23:11:30 +0900

    Topic-3

---
+++ src/test/java/zerobase/Topic3/Topic3ApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.Topic3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Topic3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
